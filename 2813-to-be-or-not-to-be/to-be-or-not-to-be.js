07d794eee73eae95739bf77b445ead3a5449def6
SYED MOHAMMAD MEHDI <48373772+SYEDMOHAMMADMEHDI@users.noreply.github.com>
2025-04-13 18:42:10 +0530

    Time: 30 ms (97.89%) | Memory: 53.4 MB (49.58%) - LeetSync

--- 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -2,14 +2,19 @@
  * @param {string} val
  * @return {Object}
  */
-var expect = function(val) {
+const expect = (val) => {
     const throwError = (errorStr) => {throw new Error(errorStr)};
+    
     return {
-        toBe : (value) => val === value || throwError('Not Equal'),
-        notToBe: (value) =>  val !== value || throwError('Equal')
-    }
+        toBe:    (val2) => val2 === val || throwError('Not Equal'),
+        notToBe: (val2) => val2 !== val || throwError('Equal'),
+    };
 };
 
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
 /**
  * expect(5).toBe(5); // true
  * expect(5).notToBe(5); // throws "Equal"

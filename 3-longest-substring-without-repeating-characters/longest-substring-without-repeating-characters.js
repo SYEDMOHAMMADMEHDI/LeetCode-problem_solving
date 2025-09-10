dbd2b114db0e6334ff739ae3d68dd81eeeb8b863
SYED MOHAMMAD MEHDI <48373772+SYEDMOHAMMADMEHDI@users.noreply.github.com>
2025-09-10 16:42:09 +0530

    Time: 7 ms (60.03%) | Memory: 56.4 MB (87.83%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -5,33 +5,18 @@
 
 //  using sliding windown and set
 var lengthOfLongestSubstring = function(s) {
-    // let left = 0;
-    // let maxLength = 0;
-    // let charSet = new Set();
-
-    // for(let right = 0; right < s.length; right++) {
-    //     while(charSet.has(s[right])) {
-    //         charSet.delete(s[left]);
-    //         left ++;
-    //     }
-    //     charSet.add(s[right]);
-    //     maxLength = Math.max(maxLength, right - left + 1);
-    // }
-    // return maxLength;
-
-    let l = 0;
-    let maxLen = 0;
+    let left = 0;
+    let maxLength = 0;
     let charSet = new Set();
 
-    for(r=0; r < s.length; r++) {
-        while(charSet.has(s[r])){
-            charSet.delete(s[l]);
-            l++;
+    for(let right = 0; right < s.length; right++) {
+        while(charSet.has(s[right])) {
+            charSet.delete(s[left]);
+            left ++;
         }
-        charSet.add(s[r]);
-        maxLen = Math.max(maxLen, r - l + 1); 
+        charSet.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
     }
-
-    return maxLen;
+    return maxLength;
     
 };

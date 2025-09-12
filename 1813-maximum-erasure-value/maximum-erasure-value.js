11ae7f73418df42301c9fe03d9cfcc7d88a1031d
SYED MOHAMMAD MEHDI <48373772+SYEDMOHAMMADMEHDI@users.noreply.github.com>
2025-09-12 16:07:33 +0530

    Time: 56 ms (45.41%) | Memory: 71.8 MB (73.49%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.js
+++ 1813-maximum-erasure-value/maximum-erasure-value.js
@@ -4,19 +4,37 @@
  */
 //  using sliding eindow and set
 var maximumUniqueSubarray = function(nums) {
-    let l = 0, r = 0, sum = 0, maxSum = 0;
+    let l = 0,  sum = 0, maxSum = 0;
     let set = new Set();
 
-    while(r < nums.length){
-        if(!set.has(nums[r])) {
-            set.add(nums[r]);
-            sum += nums[r++];
-            maxSum = Math.max(maxSum, sum);
-        }
-        else {
-            set.delete(nums[l]);
-            sum -= nums[l++];
-        }
+    // Approach using while loop :
+
+    // while(r < nums.length){
+    //     if(!set.has(nums[r])) {
+    //         set.add(nums[r]);
+    //         sum += nums[r++];
+    //         maxSum = Math.max(maxSum, sum);
+    //     }
+    //     else {
+    //         set.delete(nums[l]);
+    //         sum -= nums[l++];
+    //     }
+    // }
+
+    // Approach using for loop:
+
+    for (let r = 0; r < nums.length; r++) {
+    // if nums[r] is already in the window, shrink left until it's removed
+    while (set.has(nums[r])) {
+        set.delete(nums[l]);
+        sum -= nums[l];
+        l++;
     }
-    return maxSum;
+
+    // now it's safe to add nums[r]
+    set.add(nums[r]);
+    sum += nums[r];
+    maxSum = Math.max(maxSum, sum);
+}
+return maxSum;
 };

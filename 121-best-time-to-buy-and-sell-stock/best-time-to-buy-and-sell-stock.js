8e8c4748766bfd7a598f488de82e25b57ff2c3c0
SYED MOHAMMAD MEHDI <48373772+SYEDMOHAMMADMEHDI@users.noreply.github.com>
2025-10-16 16:05:11 +0530

    Time: 3 ms (60.47%) | Memory: 65.8 MB (38.47%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,13 +3,23 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let lowestPrice = prices[0];
-    let profit = 0;
-    for(let i=1;i<prices.length;i++) {
-        if(lowestPrice > prices[i]){
+    // Input: prices = [7,1,5,3,6,4] Output: 5 --
+    // this is a single-pass linear scan (not sliding window) 
+    //  (O(n) time, O(1) space).
+
+    let lowestPrice = Infinity;
+    let maxProfit = 0;
+
+   
+
+    for (let i = 0; i < prices.length; i++) {
+        if(prices[i] < lowestPrice) {
             lowestPrice = prices[i];
         }
-        profit = Math.max(profit, prices[i] - lowestPrice);
+        else if (prices[i] - lowestPrice > maxProfit) {
+            maxProfit = prices[i] - lowestPrice;
+        }
     }
-    return profit;
+    return maxProfit;
+
 };

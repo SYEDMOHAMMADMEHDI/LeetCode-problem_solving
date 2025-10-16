2160a5845345634dd83aebf3d3ce230344bdfe61
SYED MOHAMMAD MEHDI <48373772+SYEDMOHAMMADMEHDI@users.noreply.github.com>
2025-10-16 15:26:39 +0530

    Time: 0 ms (100.00%) | Memory: 57 MB (26.70%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,24 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // Beginner friendly code , this also works fine
-    // for(let i=0; i < nums.length; i++) {
-    //     for(let j=i+1; j < nums.length; j++) {
-    //         if(nums[i]+nums[j] == target){
-    //          return [i,j];   
-    //         }
-    //     }
-    // }
+    //Input: nums = [2,7,11,15], target = 9
+    // using hashmap
 
-    //below code using map
-    const checked = new Map();
-    for (let i=0; i < nums.length; i++){
-        const num =nums[i];
-        const complement = target - num;
-        if(checked.has(complement)){
-            return [checked.get(complement),i]
+    const numMap = {};
+    for(let i=0; i < nums.length; i++) {
+        const comp = target - nums[i];
+        if(comp in numMap) {
+            return [numMap[comp],i];
         }
-        checked.set(num,i);
+        numMap[nums[i]] = i;
     }
-
+    
 };

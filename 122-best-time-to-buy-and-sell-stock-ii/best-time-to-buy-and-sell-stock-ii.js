c3c70f5e59c48b448774fd0b0644a0bc6858889f
SYED MOHAMMAD MEHDI <48373772+SYEDMOHAMMADMEHDI@users.noreply.github.com>
2025-10-16 22:16:10 +0530

    Time: 1 ms (71.04%) | Memory: 54.1 MB (81.83%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let profit = 0;
-    for(let i=1;i<prices.length;i++){
-        if(prices[i] > prices[i-1]){
-            profit += prices[i] - prices[i-1];
+    let maxProfit = 0;
+    for(let i = 1; i < prices.length; i++){
+        if(prices[i] > prices[i-1]) {
+            maxProfit += prices[i] - prices[i-1]
         }
     }
-    return profit;
+    return maxProfit;
 };

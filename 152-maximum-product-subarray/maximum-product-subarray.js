c66c481540a089a35ddef6b4e980b008a322516f
SYED MOHAMMAD MEHDI <48373772+SYEDMOHAMMADMEHDI@users.noreply.github.com>
2025-10-17 22:16:55 +0530

    Time: 5 ms (34.73%) | Memory: 56.6 MB (46.96%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.js
+++ 152-maximum-product-subarray/maximum-product-subarray.js
@@ -2,17 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+//  Input: nums = [2,3,-2,4]
+// Output: 6
+// dynamic programming pattern
+
 var maxProduct = function(nums) {
     let res = Math.max(...nums);
     let curMax = 1, curMin = 1;
-
-    for (let n of nums) {
-        let temp = curMax * n;
+    for(let n of nums) {
+        temp = curMax * n;
         curMax = Math.max(temp, curMin * n, n);
         curMin = Math.min(temp, curMin * n, n);
-
-        res = Math.max(res, curMax);
+        res = Math.max(res,curMax);
     }
-
-    return res;    
+    return res;
 };

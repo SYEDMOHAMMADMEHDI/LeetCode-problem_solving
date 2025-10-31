9281b5cee6f563ac2160ed645f18715e5d04c177
SYED MOHAMMAD MEHDI <48373772+SYEDMOHAMMADMEHDI@users.noreply.github.com>
2025-10-31 12:14:10 +0530

    Time: 6 ms (22.59%) | Memory: 55.2 MB (92.65%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -2,23 +2,27 @@
  * @param {string} s
  * @return {boolean}
  */
+
+//  using stack LIFO
 var isValid = function(s) {
-    let stack = [];
-    let brackets = {
-        '{':'}',
-        '(':')',
-        '[':']'
+    const stack = [];
+
+    const brackets = {
+        "(": ")",
+        "[": "]",
+        "{": "}"
     }
-    for(char of s) {
-        if(brackets[char]){
+    for(let char of s) {
+        if(brackets[char]) {
             stack.push(char);
         }
         else {
             let top = stack.pop();
-            if(!top || brackets[top] !== char){
+            if(!top || brackets[top] !== char) {
                 return false;
             }
+
         }
     }
     return stack.length === 0;
-};
+}
